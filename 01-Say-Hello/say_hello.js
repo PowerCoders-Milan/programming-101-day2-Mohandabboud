65804a1722c5b82bcadcb47f5a41c283283922b1
Mohand Abboud <mohandabboud9@gmail.com>
2021-02-19 12:15:07 +0100

    first exercise- say hello

--- 01-Say-Hello/say_hello.js
+++ 01-Say-Hello/say_hello.js
@@ -2,7 +2,17 @@
 /*
 
 here
-
+Define an input form and assign it to a text to include the requested name
+Define a variable that take a text as an input and assign it to "write your name"
+our variable called Name
+Define a text that says the greeting sentence you want for my case "Hello" 
+Print hello + the varaible "Name"
 */
 
 //insert your code below
+
+var name2;
+
+
+name2 = window.prompt('Write your name');
+window.alert('Hello ' + String(name2));
